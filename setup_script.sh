d329f6d7c0b370f8daf03de3bebdf16c253d3fd8
Retaxo <105597296+sudo-Tiger@users.noreply.github.com>
2025-02-14 21:03:54 +0100

    CHANGES

--- setup_script.sh
+++ setup_script.sh
@@ -2,31 +2,41 @@
 
 echo "Setup will begin now"
 
+
+
+install_package() {
+    local package=$1
+    echo "Installing $package"
+    sudo apt-get install "$package" -y &
+    local pid=$!
+    wait $pid
+
+    if [ $? -ne 0 ]; then
+        echo "Error: $package could not be installed."
+    else
+        echo "$package installed successfully."
+    fi
+}
+
 if [[ -f /etc/os-release ]]; then
     . /etc/os-release
     OS=$NAME
     VERSION=$VERSION_ID
     echo "You are using $OS version $VERSION"
 
-    if [[ $OS == "Ubuntu" || $OS == "Debian" ]]; then
+    if [[ $OS == *"Ubuntu"* || $OS == *"Debian"* ]]; then
         echo "Updating package list"
-        sudo apt-get update
+        sudo apt-get update -y &
         echo "Upgrading packages"
-        sudo apt-get upgrade -y
-        echo "Installing git"
-        sudo apt-get install git -y
-        echo "Installing curl"
-        sudo apt-get install curl -y
-        echo "Installing zsh"
-        sudo apt-get install zsh -y
-        echo "Installing vim"
-        sudo apt-get install vim -y
-        echo "Installing tmux"
-        sudo apt-get install tmux -y
-        echo "Installing htop"
-        sudo apt-get install htop -y
-        echo "Installing neofetch"
-        sudo apt-get install neofetch -y
+        sudo apt-get upgrade -y &
+
+        install_package git
+        install_package curl
+        install_package zsh
+        install_package vim
+        install_package tmux
+        install_package htop
+        install_package neofetch
     else
         echo "Unsupported OS"
     fi
