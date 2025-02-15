9c14516b22cbf987b569be6b9c8e261c7e23dcdc
Retaxo <105597296+sudo-Tiger@users.noreply.github.com>
2025-02-15 22:38:30 +0100

    CHANGES

--- setup_script.sh
+++ setup_script.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    OS=$NAME
+    VERSION=$VERSION_ID
+    echo "You are using $OS version $VERSION"
+else
+    echo "Unsupported OS"
+    exit 1
+fi
+
 echo "Setup will begin now"
 
-
-//Functions
-
-install_package() {
+install_package(){
     local package=$1
     echo "Installing $package"
-    sudo apt-get install "$package" -y &
-    local pid=$!
-    wait $pid
+    sudo apt-get install "$package" -y
 
     if [ $? -ne 0 ]; then
         echo "Error: $package could not be installed."
@@ -20,43 +25,38 @@ install_package() {
 }
 
 configure(){
-    if [[ -f ~/.vimrc ]]; then
-        echo "Configure"
-    else
-        touch ~/.vimrc
-    fi
-    echo "asdasdasdasd"
+    cp ./config/.vimrc ~/
 }
 
-
-
-
-// Main
-
-if [[ -f /etc/os-release ]]; then
-    . /etc/os-release
-    OS=$NAME
-    VERSION=$VERSION_ID
-    echo "You are using $OS version $VERSION"
-
-    if [[ $OS == *"Ubuntu"* || $OS == *"Debian"* ]]; then
-        echo "Updating package list"
-        sudo apt-get update -y &
-        echo "Upgrading packages"
-        sudo apt-get upgrade -y &
-
-        install_package git
-        install_package curl
-        install_package zsh
-        install_package vim
-        install_package tmux
-        install_package htop
-        install_package neofetch
+install_java(){
+    if ! java --version | grep -q "21"; then
+        echo "Installing Java 21..."
+        wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
+        sudo dpkg -i jdk-21_linux-x64_bin.deb
+        sudo apt-get install -f -y
     else
-        echo "Unsupported OS"
+        echo "Java 21 is already installed."
     fi
+}
+
+if [[ $OS == *"Debian"* || $OS == *"Ubuntu"* ]]; then
+    echo "Updating package list"
+    sudo apt-get update -y
+    echo "Upgrading packages"
+    sudo apt-get upgrade -y
+
+    install_package git
+    install_package curl
+    install_package zsh
+    install_package vim
+    install_package tmux
+    install_package htop
+    install_package neofetch
 else
     echo "Unsupported OS"
 fi
 
-configure;
+configure
+install_java
+
+echo "Operating system: $OS"
